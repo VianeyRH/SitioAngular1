e219e7354a241a14f52061909b67d8eb642a549d
Ramirez Hernandez Brenda Vianey <201821898@tese.edu.mx>
2021-03-23 20:40:44 -0600

    Se realizaron los cambios para agregar los antiheroes en lista dimamica

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,23 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+
 import {Hero} from '../hero';
+import {HEROES} from '../mock-heroes';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero ={
-    id: 1,
-    name: 'DeadPool',
-    edad: 30,
-    tippoder: 'Pelea cuerpo a cuerpo inmortalidad y regeneración',
-    identidad: 'Wade Winston Wilson',
-    universo: 'Marvel'
-  };
+
+  heroes = HEROES;
+  selectedHero?: Hero;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,72 @@
+import {Hero}from "./hero";
+
+export const HEROES: Hero[] = [
+{id: 11, 
+name: "Deadpool",
+edad: 20,
+tippoder: 'Fuerza, batalla cuerpo a cuerpo, regeneración.',
+identidad: 'Wade Wilson',
+universo: 'Marvel'},
+
+{id: 12, 
+name: "Ghost Rider",
+edad: 30,
+tippoder: 'Fuerza, fuego, resurección',
+identidad: 'Johnny Blaze',
+universo: 'Marvel'},
+
+{id: 13, 
+name: "The Punisher",
+edad: 30,
+tippoder: 'Fuerza y batallas cuerpo a cuerpo',
+identidad: 'Frank Castle',
+universo: 'Marvel'},
+
+{id: 14, 
+name: "Venom",
+edad: 40,
+tippoder: 'Fuerza y adherirse a cualquier cuerpo',
+identidad: 'Eddie Brock',
+universo: 'Marvel'},
+
+{id: 15, 
+name: "Deathstroke",
+edad: 25,
+tippoder: 'Fuerza, velocidad, estratega, entre otros.',
+identidad: 'Slade Wilson',
+universo: 'DC'},
+
+{id: 16, name: "Wolverine",
+edad: 1000,
+tippoder: 'Fuerza, batalla cuerpo a cuerpo y mutante.',
+identidad: 'James Howlett',
+universo: 'DC'},
+
+{id: 17, 
+name: "Black Widow",
+edad: 28,
+tippoder: 'Fuerza, salto a gran distancia.',
+identidad: 'Natasha Romanoff',
+universo: 'Marvel'},
+
+{id: 18, 
+name: "Deadshot", 
+edad: 28,
+tippoder: 'Mejor tirador  y habilidad sobrehumanas.',
+identidad: 'Floyd Lawton',
+universo: 'DC'},
+
+{id: 19, 
+name: "Scarlet Witch", 
+edad: 36,
+tippoder: 'Magia, control de tiempo, clarividencia, entre otros.',
+identidad: 'Wanda Maximoff',
+universo: 'Marvel'},
+
+{id: 20, 
+name: "Domino", 
+edad: 30,
+tippoder: 'Suerte',
+identidad: 'Neena Thurman',
+universo: 'Marvel'}
+];
